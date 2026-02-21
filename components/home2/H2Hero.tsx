5d6653f79ab5741e56a205c924822ffbefcb7588
Kavya Patel <kavyapatel@Kavyas-MacBook-Air.local>
2026-02-21 12:41:46 +0530

    Change button text to Our Companies

--- components/home2/H2Hero.tsx
+++ components/home2/H2Hero.tsx
@@ -68,7 +68,7 @@ export const H2Hero: React.FC<H2HeroProps> = ({ onContactClick }) => {
                 onClick={scrollToServices}
                 className="px-6 py-3 md:px-8 md:py-4 bg-insaan-black text-white text-[13.5px] md:text-[15px] font-bold rounded-xl md:rounded-2xl hover:bg-[#59CBE8] hover:text-insaan-black transition-all duration-300 shadow-lg hover:shadow-xl hover:-translate-y-0.5"
               >
-                Our Solutions
+                Our Companies
               </button>
               <button
                 onClick={() => onContactClick?.()}
